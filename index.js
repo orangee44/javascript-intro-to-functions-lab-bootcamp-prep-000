692c7b4762fa7124b699a5c06b11260eddd269d4
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-07-29 02:29:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,3 @@ function whisper(string)
 }
 
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
-  })
-})
