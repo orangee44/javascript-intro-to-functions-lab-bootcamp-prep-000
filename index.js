5f80ed24e42ca19e4d96ccb765cc392de622d533
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-07-29 02:33:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function logShout(string)
 {
   console.log(string.toUpperCase)
 }
+
+
+describe('logShout(string)', function() {
+  it('calls console.log() its one argument in all caps', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
