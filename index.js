a1bbcceae95bd424b6c6411cca3a87bd02582bbf
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-07-29 02:46:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,18 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if (string === string.toLowerCase()
+  if (string === string.toLowerCase())
   {
     return "I can't hear you!"
-  }
+  } else if (string === string.toUpperCase())
+  {
+    return "YES INDEED!"
+  } else 
   
   
 }
 
 describe('sayHiToGrandma(string)', function() {
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
