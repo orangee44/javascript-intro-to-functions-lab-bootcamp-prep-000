0b3bb94190f431ca7235c4780fc8fb6a27592264
jnutmegger@gmail.com <jnutmegger@gmail.com>
2018-07-29 02:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function shout (string)
+function shout(string)
 {
   return string.toUpperCase()
 }
+
+function whisper(string)
+{
+  
+}
+
+
+describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})
